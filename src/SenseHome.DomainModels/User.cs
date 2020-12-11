c0ff796f281520222b7fab3cf5b73600943ed5a1
Rafiul Islam <avoidcloud@gmail.com>
2020-12-11 22:20:58 +0600

    common types package referencing from domain model project and removed commented attributes from user model

--- src/SenseHome.DomainModels/User.cs
+++ src/SenseHome.DomainModels/User.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-//using SenseHome.Common.Enums;
+using SenseHome.Common.Enums;
 using SenseHome.DomainModels.Base;
 
 namespace SenseHome.DomainModels
@@ -9,7 +9,7 @@ namespace SenseHome.DomainModels
     {
         public string Name { get; set; } //TODO: User Name will be unique
         public string Password { get; set; }
-        //public UserType Type { get; set; }
+        public UserType Type { get; set; }
         public bool IsActive { get; set; } = true;
         public DateTime LastConnected { get; set; } = DateTime.MinValue;
         public List<DateTime> Logs { get; set; }
