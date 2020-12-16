b802d614fba83ae55530a67b6956815b7e8342f6
Rafiul Islam <avoidcloud@gmail.com>
2020-12-16 22:35:04 +0600

    update dto library

--- src/SenseHome.API/Controllers/UserController.cs
+++ src/SenseHome.API/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SenseHome.DataTransferObjects.User;
@@ -36,7 +35,5 @@ namespace SenseHome.API.Controllers
             var users = await userService.GetAllAsync();
             return Ok(users);
         }
-
-
     }
 }
