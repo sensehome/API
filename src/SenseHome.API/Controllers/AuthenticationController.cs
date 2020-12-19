71faaa3f853536aac451dce366f6dd3f12854d35
Rafiul Islam <avoidcloud@gmail.com>
2020-12-19 18:43:27 +0600

    fixed authentication controller and added precondition while creating a new user

--- src/SenseHome.API/Controllers/AuthenticationController.cs
+++ src/SenseHome.API/Controllers/AuthenticationController.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using SenseHome.DataTransferObjects.Authentication;
 using SenseHome.Services.Authentication;
 
 namespace SenseHome.API.Controllers
 {
     [Route("api/auth")]
-    [ApiController]
     public class AuthenticationController : SenseHomeBaseController
     {
         private readonly IAuthenticationService authenticationService;
@@ -23,27 +14,19 @@ namespace SenseHome.API.Controllers
         {
             this.authenticationService = authenticationService;
         }
-        [HttpPost, Route("login")]
-        public async Task<ActionResult<String>> LoginAsync([FromBody] UserLoginDto userLogin)
+
+        [HttpPost("login")]
+        public async Task<ActionResult<TokenDto>> LoginAsync([FromBody] UserLoginDto userLogin)
         {
-                var user = await authenticationService.Authenticate(userLogin.Name, userLogin.Password);
-                if (user == null)
-                {
-                    return Unauthorized("Invalid credential");
-                }
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var token = await authenticationService.LoginAsync(userLogin);
+            return Ok(token);
+        }
 
-                var tokenOptions = new JwtSecurityToken(
-                    issuer: "https://localhost:44333",
-                    audience: "https://localhost:44333",
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signingCredentials
-                    );
-
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
-                return Ok(new { Token = tokenString });
-            }
+        [HttpPost("internal")]
+        public async Task<ActionResult<TokenDto>> InternalLoginAsync([FromBody] InternalLoginDto internalLogin)
+        {
+            var token = await authenticationService.InternalLoginAsync(internalLogin);
+            return Ok(token);
         }
     }
+}
