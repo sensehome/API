be382e82b9de233eca3d41ed856411e91c35fd91
Rafiul Islam <avoidcloud@gmail.com>
2020-12-20 14:25:32 +0600

    added profile and motion detection services

---
+++ src/SenseHome.Services/MotionDetection/IMotionDetectionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SenseHome.DataTransferObjects.MotionDetection;
+
+namespace SenseHome.Services.MotionDetection
+{
+    public interface IMotionDetectionService
+    {
+        Task<IEnumerable<MotionDetectionDto>> GetAllEntitiesAsync();
+    }
+}
